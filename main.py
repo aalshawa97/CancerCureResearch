a7a7ba2fa7889c39a7e926ee15a38a46ed305c56
Abdullah Mutaz Alshawa <abdullahalshawa@gmail.com>
2023-11-25 11:38:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-#https://www.youtube.com/watch?v=z1PGJ9quPV8&t=284s
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
 
 # Load the dataset
 dataset = pd.read_csv('cancer.csv')
 
 # Separate features (x) and target variable (y)
-x = dataset.drop(["diagnosis(1=m, 0=b)"], axis=1)  # Specify axis=1 to drop the column
+x = dataset.drop(["diagnosis(1=m, 0=b)"], axis=1)
 y = dataset["diagnosis(1=m, 0=b)"]
+
+# Data preprocessing
+scaler = StandardScaler()
+x = scaler.fit_transform(x)
 
 # Split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 # Build the model
-model = tf.keras.models.Sequential()
-
-# Input layer with 256 neurons and input shape equal to the number of features
-model.add(tf.keras.layers.Dense(256, input_shape=(x_train.shape[1],), activation='sigmoid'))
-# Hidden layer with 256 neurons
-model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
-# Output layer with 1 neuron (binary classification)
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model = Sequential([
+    Dense(256, input_shape=(x_train.shape[1],), activation='relu'),
+    Dropout(0.5),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')
+])
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+# Define callbacks
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+lr_schedule = LearningRateScheduler(lambda epoch: 1e-3 * 0.9 ** epoch)
+
 # Train the model
-model.fit(x_train, y_train, epochs=1000)
+history = model.fit(x_train, y_train, epochs=100, validation_split=0.2, callbacks=[early_stopping, lr_schedule])
 
 # Evaluate the model on the test set
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
